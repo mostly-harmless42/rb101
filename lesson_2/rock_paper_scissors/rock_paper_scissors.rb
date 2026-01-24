493ff18de8a436917b81e238fcefab955735d610
Will Read <wrgread1@gmail.com>
2026-01-24 13:32:25 -0800

    Add best of three functionality

--- lesson_2/rock_paper_scissors/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors/rock_paper_scissors.rb
@@ -14,21 +14,11 @@ def win?(first, second)
   WIN[first].include?(second)
 end
 
-def display_results(player, computer)
-  if win?(player, computer)
-    prompt("You won!")
-  elsif win?(computer, player)
-    prompt("Computer won!")
-  else
-    prompt("It's a tie!")
-  end
-end
-
 def abbreviated_choice(str)
   CHOICES.find { |choice| choice.start_with?(str) }
 end
 
-loop do
+def player_choice
   player = ''
   loop do
     prompt("Choose one: #{CHOICES.join(', ')}")
@@ -37,13 +27,47 @@ loop do
     break if CHOICES.include?(player)
     puts("That's not a valid choice.")
   end
+  player
+end
 
-  computer = CHOICES.sample
+def display_round_results(player, computer)
+  if win?(player, computer)
+    prompt("You won the round!")
+  elsif win?(computer, player)
+    prompt("Computer won the round!")
+  else
+    prompt("It's a tie!")
+  end
+end
 
-  prompt("You chose: #{player}. Computer chose: #{computer}.")
+loop do
+  prompt("Welcome to Rock Paper Scissors! Best of 3 wins!")
 
-  display_results(player, computer)
+  player_score = 0
+  computer_score = 0
 
+  loop do
+    puts
+    prompt("Player: #{player_score}  Computer: #{computer_score}")
+    player = player_choice()
+    computer = CHOICES.sample
+
+    prompt("You chose: #{player}. Computer chose: #{computer}.")
+    display_round_results(player, computer)
+
+    player_score += 1 if win?(player, computer)
+    computer_score += 1 if win?(computer, player)
+
+    break if player_score == 3 || computer_score == 3
+  end
+
+  puts
+  prompt("Final scores:")
+  prompt("Player: #{player_score} vs Computer: #{computer_score}")
+  winner = player_score == 3 ? 'You' : 'Computer'
+  prompt("#{winner} won the game!")
+
+  puts
   prompt("Do you want to play again? (Y to play again)")
   answer = gets.chomp
   break unless answer.downcase == 'y'
